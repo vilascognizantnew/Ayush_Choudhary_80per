761a374afd4361460160454c7cfbc226367a7af3
vilascognizantnew <72960946+vilascognizantnew@users.noreply.github.com>
2020-10-19 16:22:45 +0530

    Update StockServiceImpl.java

--- Stock/src/main/java/com/example/Stock/Service/StockServiceImpl.java
+++ Stock/src/main/java/com/example/Stock/Service/StockServiceImpl.java
@@ -1,5 +1,31 @@
-package com.example.Stock.Service;
+	
+package com.example.Stock.service;
+	
 
-public class StockServiceImpl {
+	import java.util.List;
+	
 
-}
+	import com.cts.demo.dao.StockDao;
+	import com.cts.demo.dao.StockDaoImpl;
+	import com.cts.demo.model.Stock;
+	
+
+	public class StockServiceImpl implements StockService{
+	
+
+		StockDao stockDao=new StockDaoImpl();
+		@Override
+		public List<Stock> getAllStock() {
+			// TODO Auto-generated method stub
+			return stockDao.getAllStock();
+		}
+	
+
+		@Override
+		public Stock getStockById(int id) {
+			// TODO Auto-generated method stub
+			return stockDao.getStockById(id);
+		}
+	
+
+
