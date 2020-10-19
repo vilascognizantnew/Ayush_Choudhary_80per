518303403bf36332bb07a340e5d4d2b4ad65f6a0
vilascognizantnew <72960946+vilascognizantnew@users.noreply.github.com>
2020-10-19 16:16:31 +0530

    Update StockController.java

--- Stock/src/main/java/com/example/Stock/controller/StockController.java
+++ Stock/src/main/java/com/example/Stock/controller/StockController.java
@@ -1,5 +1,78 @@
 package com.example.Stock.controller;
 
-public class StockController {
 
-}
+	
+
+	
+
+	
+
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.http.HttpStatus;
+	import org.springframework.http.ResponseEntity;
+	import org.springframework.web.bind.annotation.GetMapping;
+	import org.springframework.web.bind.annotation.PathVariable;
+	import org.springframework.web.bind.annotation.PostMapping;
+	import org.springframework.web.bind.annotation.PutMapping;
+	import org.springframework.web.bind.annotation.RequestBody;
+	import org.springframework.web.bind.annotation.RestController;
+	
+
+	import com.example.Stock.model.Stock;
+	import com.example.Stock.service.StockService;
+	import com.example.Stock.service.StockServiceImpl;
+	
+
+	@RestController
+	public class StockController {
+		@Autowired
+		PermanentStockService StockService;
+		
+		public StockController() {
+					
+		}
+		
+		@GetMapping(value = "/StockList")
+		public ResponseEntity<Object> StockList()
+		{
+			
+			return new ResponseEntity<>(StockDao.getAllStock(),HttpStatus.OK);
+		}
+		
+		@GetMapping(value = "/getProductById/id/{id}/name/{name}")
+		public ResponseEntity<Object> StockList(@PathVariable("id") Integer id)
+		{
+			
+				Stock Stock=StockDao.getStockById(id);
+				if(Stock!=null) {
+					
+					return new ResponseEntity<>(Stock,HttpStatus.OK);	
+				}
+				else
+				return new ResponseEntity<>("Resord Not Found",HttpStatus.OK);
+					
+		}
+		
+		
+		@PostMapping(value = "/addStock")
+		public ResponseEntity<Object> addStock(@RequestBody Stock Stock)
+		{
+			
+			return new ResponseEntity<>(StockDao.addStock(Stock),HttpStatus.OK);	
+		}
+		
+		@PutMapping(value = "/updateStock")
+		public ResponseEntity<Object> updateStock(@RequestBody Stock Stock)
+		{
+			Stock StockTemp=StockDao.updateStock(Stock);
+			if(StockTemp!=null) {
+				
+				return new ResponseEntity<>(StockTemp,HttpStatus.OK);	
+			}
+			else
+			return new ResponseEntity<>("Resord Not Found",HttpStatus.OK);
+		}
+	
+
+	}
+
